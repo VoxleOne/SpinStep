fd8f2f3ce4adabfd80c2a3e20d2f755021f6e496
Eraldo Marques <119956342+VoxLeone@users.noreply.github.com>
2025-05-13 17:11:38 -0300

    Delete spinstep/node.py

--- spinstep/node.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, name, orientation, children=None):
-        self.name = name
-        self.orientation = orientation  # [x, y, z, w]
-        self.children = children if children else []
