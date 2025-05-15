3588bba6a47b8fb4a4ffa788e3cf294ebdf4aa4f
Eraldo Marques <119956342+VoxLeone@users.noreply.github.com>
2025-05-15 12:03:30 -0300

    Update and rename test.py to test_spinstep.py

--- tests/test.py
+++
@@ -1,3 +0,0 @@
-# test.py — MIT License
-# Author: Eraldo Marques <eraldo.bernardo@gmail.com> — Created: 2025-05-14
-# See LICENSE.txt for full terms. This header must be retained in redistributions.
