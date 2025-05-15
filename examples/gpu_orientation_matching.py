2af674d835e704870e85debf65a898eaaeb8cae5
Eraldo Marques <119956342+VoxLeone@users.noreply.github.com>
2025-05-15 09:15:25 -0300

    Update gpu_orientation_matching.py

--- examples/gpu_orientation_matching.py
+++ examples/gpu_orientation_matching.py
@@ -1,1 +1,13 @@
-i
+import numpy as np
+from spinstep.discrete import DiscreteOrientationSet
+from spinstep.utils.quaternion_math import batch_quaternion_angle
+
+N = 100_000
+orientations = np.random.randn(N, 4)
+orientation_set = DiscreteOrientationSet(orientations, use_cuda=True)
+
+query = np.array([[0, 0, 0, 1]])
+xp = orientation_set.xp
+angles = batch_quaternion_angle(xp.array(query), orientation_set.orientations, xp)
+close_inds = xp.where(angles[0] < 0.1)[0]
+print(f"Found {close_inds.size} close orientations (on GPU)")
